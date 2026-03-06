9796fa8796b50a85d4a34e0e5f613c32a3839bab
tguygeek <ecladorguy@gmail.com>
2026-03-06 16:01:43 +0100

    Ajoute Procfile pour Railway

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,11 +15,12 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:5173,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort(),
-        // Sanctum::currentRequestHost(),
+    'stateful' => explode(',', env(
+        'SANCTUM_STATEFUL_DOMAINS', 
+        sprintf(
+            '%s%s',
+            'localhost,localhost:5173,127.0.0.1',
+            env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''        // Sanctum::currentRequestHost(),
     ))),
 
     /*
